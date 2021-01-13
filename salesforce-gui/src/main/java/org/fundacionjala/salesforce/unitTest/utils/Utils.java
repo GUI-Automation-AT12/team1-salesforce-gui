d97ca094391ff36f76da490f09f2cfa64d691c16
Samuel Loza <starsaminf@gmail.com>
2021-01-12 21:15:11 -0400

    Add base64 decode to utils class

---
+++ salesforce-gui/src/main/java/org/fundacionjala/salesforce/unitTest/utils/Utils.java
@@ -0,0 +1,65 @@
+package org.fundacionjala.salesforce.unitTest.utils;
+
+import org.fundacionjala.salesforce.constants.Constants;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Base64;
+
+/**
+ * [SL] This class contains utilities for the project.
+ */
+public final class Utils {
+    /**
+     * [SL] Constructor.
+     */
+    private Utils() {
+
+    }
+    /**
+     * [SL] Returns a text encode in base64 in plain text.
+     * @param textEncode
+     * @return a plain text.
+     */
+    public static String base64Decode(final String textEncode) {
+        byte[] bytes = new byte[0];
+        try {
+            bytes = textEncode.getBytes("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+        String textDecode = new String(Base64.getDecoder().decode(bytes));
+        if (isValidString(textDecode)) {
+            return textDecode;
+        } else {
+            return Constants.INVALID_TEXT_ENCODE;
+        }
+    }
+
+    /**
+     * [SL] Check if the string contains only printable characters.
+     * @param value
+     * @return if the string is printable.
+     */
+    public static boolean isValidString(final String value) {
+        if (value == null) {
+            return false;
+        }
+        for (int i = 0; i < value.length(); i++) {
+            if (!isPrintable(value.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * [SL] Check if the character is printable.
+     * @param character
+     * @return true if between 32 and 126 inclusive
+     */
+    public static boolean isPrintable(final char character) {
+        return character >= Constants.ASCII_PRINTABLE_START && character <= Constants.ASCII_PRINTABLE_END;
+    }
+}
---
+++ salesforce-gui/src/test/java/org/fundacionjala/salesforce/unitTest/utils/UtilsTest.java
@@ -0,0 +1,77 @@
+package org.fundacionjala.salesforce.unitTest.utils;
+
+import org.fundacionjala.salesforce.constants.Constants;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+/**
+ * [SL] This class contains test for base64Decode.
+ */
+public class UtilsTest {
+    /**
+     * Test to decode a text in base64.
+     */
+    @Test
+    public void base64DecodeValidCode() {
+        String textEncode = "SGVsbG8gV29ybGQuLi4gOik=";
+        String actual = Utils.base64Decode(textEncode);
+        String expected = "Hello World... :)";
+        Assert.assertEquals(actual, expected);
+    }
+
+    /**
+     * Test to decode a wrong text in base64.
+     */
+    @Test
+    public void base64DecodeInvalidCode() {
+        String textEncode = "SVsbG8gV29ybGQuLi4Oi";
+        String actual = Utils.base64Decode(textEncode);
+        String expected = Constants.INVALID_TEXT_ENCODE;
+        Assert.assertEquals(actual, expected);
+    }
+
+    /**
+     * Test to check if the ascii is printable.
+     */
+    @Test
+    public void asciiPrintableCharacter() {
+        String message = "This character '%c' is not printable";
+        SoftAssert softAssertion = new SoftAssert();
+        for (char ch = Constants.ASCII_PRINTABLE_START; ch <= Constants.ASCII_PRINTABLE_END; ch++) {
+            softAssertion.assertTrue(Utils.isPrintable(ch), String.format(message, ch));
+        }
+        softAssertion.assertAll();
+    }
+
+    /**
+     * Test to check if the ascii is not printable.
+     */
+    @Test
+    public void asciiNotPrintableCharacter() {
+        String message = "This character '%c' is printable";
+        SoftAssert softAssertion = new SoftAssert();
+        for (char ch = 0; ch < Constants.ASCII_PRINTABLE_START; ch++) {
+            softAssertion.assertFalse(Utils.isPrintable(ch), String.format(message, ch));
+        }
+        softAssertion.assertAll();
+    }
+
+    /**
+     * Test to check if the string contains valid characters.
+     */
+    @Test
+    public void textContainsValidCharacters() {
+        String message = "ABCDEF%$.1234567890-.m";
+        Assert.assertTrue(Utils.isValidString(message));
+    }
+
+    /**
+     * Test to check if the string contains invalid characters.
+     */
+    @Test
+    public void textContainsInvalidCharacters() {
+        String message = "Ĩĩİ";
+        Assert.assertFalse(Utils.isValidString(message));
+    }
+}
