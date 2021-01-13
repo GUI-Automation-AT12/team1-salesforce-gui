1000b02c50db7050cd476d4e5fb502a3e36a55d9
Samuel Loza <starsaminf@gmail.com>
2021-01-13 16:02:02 -0400

    Add Constants.java

---
+++ salesforce-gui/src/main/java/org/fundacionjala/salesforce/constants/Constants.java
@@ -0,0 +1,20 @@
+package org.fundacionjala.salesforce.constants;
+
+/**
+ * [SL] This class offers constant for the salesforce-gui.
+ */
+public final class Constants {
+    /**
+     * Constructor.
+     */
+    private Constants() {
+
+    }
+
+    /** Encode/Decode Constants. **/
+    public static final String INVALID_TEXT_ENCODE = "Invalid encode text.";
+
+    /** ASCII Printable. **/
+    public static final int ASCII_PRINTABLE_START = 32;
+    public static final int ASCII_PRINTABLE_END = 126;
+}
