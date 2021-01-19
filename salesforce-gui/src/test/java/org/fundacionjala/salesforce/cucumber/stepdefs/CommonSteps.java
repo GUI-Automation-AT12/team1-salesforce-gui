1d274515dcdb860b02d1a1e37d919c7a80452a67
Samuel Loza <starsaminf@gmail.com>
2021-01-19 17:19:34 -0400

    Add CommonSteps class

---
+++ salesforce-gui/src/test/java/org/fundacionjala/salesforce/cucumber/stepdefs/CommonSteps.java
@@ -0,0 +1,23 @@
+package org.fundacionjala.salesforce.cucumber.stepdefs;
+
+import io.cucumber.java.en.When;
+import org.fundacionjala.salesforce.utils.PageTransporter;
+
+import java.net.MalformedURLException;
+
+/**
+ * This class contains the CommonSteps.
+ */
+public class CommonSteps {
+
+    /**
+     * Goes to specific page.
+     *
+     * @param page
+     * @throws MalformedURLException
+     */
+    @When("I go to {string}")
+    public void goTo(final String page) throws MalformedURLException {
+        PageTransporter.navigateToPage(page);
+    }
+}
