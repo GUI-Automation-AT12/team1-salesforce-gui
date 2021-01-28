067b9b3638a4e5ab15a8352b5da36178c933ffdb
Samuel Loza <starsaminf@gmail.com>
2021-01-28 17:30:05 -0400

    Fix redundant import in AccountHooks

--- salesforce-gui/src/test/java/org/fundacionjala/salesforce/cucumber/hooks/AccountHooks.java
+++ salesforce-gui/src/test/java/org/fundacionjala/salesforce/cucumber/hooks/AccountHooks.java
@@ -16,7 +16,6 @@ import org.fundacionjala.salesforce.utils.Setup;
 
 import java.io.IOException;
 import org.fundacionjala.salesforce.utils.CSVUtils;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
